e3182dbdc7a34fa83e5898b544ea87edcc59933c
Rennan Oliveira <89608830+Rennoi123@users.noreply.github.com>
2023-05-08 10:53:59 -0300

    Inicio da aplicação com todas as dependencia.

---
+++ BarberTimeSoft/src/main/java/BarberTimeSoft/BarberTimeSoft/BarberTimeSoftApplication.java
@@ -0,0 +1,13 @@
+package BarberTimeSoft.BarberTimeSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberTimeSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberTimeSoftApplication.class, args);
+	}
+
+}
---
+++ BarberTimeSoft/src/test/java/BarberTimeSoft/BarberTimeSoft/BarberTimeSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package BarberTimeSoft.BarberTimeSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberTimeSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
